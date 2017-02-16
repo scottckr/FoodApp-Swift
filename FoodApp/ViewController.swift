4160e2009b3bff4fa8721ae118aad43150f7454e
Scott Crocker <scottckr@hotmail.com>
2017-02-16 21:38:52 +0100

    Initial Commit

---
+++ FoodApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodApp
+//
+//  Created by Scott Crocker on 2017-02-16.
+//  Copyright © 2017 Scott Crocker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
