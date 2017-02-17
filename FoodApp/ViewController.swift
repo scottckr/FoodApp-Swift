218fd85fa8b5cb87e42d205549bd227f57932e2f
Scott Crocker <scottckr@hotmail.com>
2017-02-17 23:03:00 +0100

    Basic layout and segues. DO NOT CLICK SEARCH BAR, APP WILL CRASH!

--- FoodApp/ViewController.swift
+++ FoodApp/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var itemNameLabel: UILabel!
+    var itemName: String = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        itemNameLabel.text = itemName
     }
 
     override func didReceiveMemoryWarning() {
